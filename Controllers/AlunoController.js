4ed5dbb6a20e740d9d88a7723b496f7c6b071860
Fernando Hieda <58814238+Igashe@users.noreply.github.com>
2022-08-24 14:03:46 -0300

    AlunoController update
    AlunoController tem um método de contruir um aluno

--- Controllers/AlunoController.js
+++ Controllers/AlunoController.js
@@ -1,6 +1,11 @@
 const IAluno = require("../Model/InterfaceAluno")
 const FactoryAluno = require("../Model/FactoryAluno")
-
-module.exports = class Controller {
+module.exports = class ControllerAluno {
     //funcionalidades da classe Aluno
+    FAluno = new FactoryAluno
+    IAluno = new IAluno
+    criaAluno(nome, ira, id, perfil, statusMatricula) {
+        return this.FAluno.criaAluno(nome, ira, id, perfil, statusMatricula)
+    }
+        
 }
