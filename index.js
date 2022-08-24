ec2c51ae2900a0bef62e7aa5db044d33ea94cc84
Fernando Hieda <58814238+Igashe@users.noreply.github.com>
2022-08-24 13:39:24 -0300

    package.json

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
-// const Aluno = require("./Aluno")
-const FactoryDisciplina = require("./Model/FactoryDisciplina")
-const FactoryAluno = require("./Model/FactoryAluno")
+//index.js
+const http = require('http'); 
+const express = require('express'); 
+const app = express(); 
+ 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+ 
+app.get('/', (req, res, next) => {
+    res.json({message: "Tudo ok por aqui!"});
+})
+ 
+app.get('/clientes', (req, res, next) => { 
+    console.log("Retornou todos clientes!");
+    res.json([{id:1,nome:'luiz'}]);
+}) 
+ 
+const server = http.createServer(app); 
+server.listen(3000);
+console.log("Servidor escutando na porta 3000...")
 
-// const aluno1 = new Aluno("Matheus", 14000, 123, 2019, "Ativo")
-const FDisciplina = new FactoryDisciplina
-const disciplina1 = FDisciplina.criaDisciplina("Mat", 14, "Mat")
-const disciplina2 = FDisciplina.criaDisciplina("Fis", 20, "Mat")
 
-const FAluno = new FactoryAluno
-const aluno2 = FAluno.criaAluno("Matheus", 14000, 123, 2019, "Ativo")
+// const FactoryDisciplina = require("./Model/FactoryDisciplina")
+// const FactoryAluno = require("./Model/FactoryAluno")
 
-// console.log(aluno1)
-console.log(disciplina1)
-// console.log(disciplina2)
-console.log(aluno2)
+// // const aluno1 = new Aluno("Matheus", 14000, 123, 2019, "Ativo")
+// const FDisciplina = new FactoryDisciplina
+// const disciplina1 = FDisciplina.criaDisciplina("Mat", 14, "Mat")
+// const disciplina2 = FDisciplina.criaDisciplina("Fis", 20, "Mat")
 
-aluno2.newDisciplina(disciplina1.nome)
-aluno2.newDisciplina(disciplina2.nome)
+// const FAluno = new FactoryAluno
+// const aluno2 = FAluno.criaAluno("Matheus", 14000, 123, 2019, "Ativo")
 
-// console.log(aluno1)
-console.log(aluno2.allDisciplinasInscritas)
+// // console.log(aluno1)
+// console.log(disciplina1)
+// // console.log(disciplina2)
+// console.log(aluno2)
+
+// aluno2.newDisciplina(disciplina1.nome)
+// aluno2.newDisciplina(disciplina2.nome)
+
+// // console.log(aluno1)
+// console.log(aluno2.allDisciplinasInscritas)
