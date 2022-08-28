1d3fc99c96b1c1225684395641d64a0e337ab34d
Fernando Hieda <58814238+Igashe@users.noreply.github.com>
2022-08-28 17:08:18 -0300

    Update

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolovido com <i className="fa fa-heart text-danger"></i> por
+            <strong> POOA <span className="text-danger">grupo </span>6</strong>
+        </span>
+    </footer>
---
+++ frontend/src/components/template/Nav.jsx
@@ -0,0 +1,16 @@
+import './Nav.css'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default props =>
+    <aside className="manu-area">
+        <nav className="menu">
+            {/*<i className="fa fa-home">*/}
+            <Link to="/">
+                <i className="fa fa-home"></i> Ínicio
+            </Link>
+            <Link to="/users">
+                <i className="fa fa-users"></i> Usuários
+            </Link>
+        </nav>
+    </aside>
