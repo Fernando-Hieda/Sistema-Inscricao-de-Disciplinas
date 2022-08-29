f33e13d58254cf5259c8ee62da5a6be2e921423e
brunomatosdesouza <107889891+brunomatosdesouza@users.noreply.github.com>
2022-08-29 18:39:45 -0300

    remoção de contr e fac de disciplina

--- Controllers/DisciplinaController.js
+++
@@ -1,6 +0,0 @@
-const IDisciplina= require("../Model/InterfaceDisciplina")
-const FactoryDisciplina= require("../Model/FactoryDisciplina")
-
-module.exports = class Controller {
-    //funcionalidades da classe Disciplina
-}
