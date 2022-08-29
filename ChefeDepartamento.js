d8a3e4755ac77568956446a2586ec1689beba919
Fernando Hieda <58814238+Igashe@users.noreply.github.com>
2022-08-29 18:03:31 -0300

    Delete chefeDepartamento

--- ChefeDepartamento.js
+++
@@ -1,8 +0,0 @@
-const Disciplina = require("./Model/Disciplina")
-
-module.exports = class ChefeDepartamento {
-    //propriedades e funções da classe
-    constructor(nome) {
-        this.nome = nome
-    }
-}
