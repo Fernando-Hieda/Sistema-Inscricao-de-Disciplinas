59a9b200571b5e2796ec738db08c197d54980ac8
João Bueno <joaovictorbuenodecaldas@gmail.com>
2022-08-31 19:34:41 -0300

    Upload inicial state

---
+++ Model/State.js
@@ -0,0 +1,161 @@
+module.exports = class State {
+    constructor() {
+    
+    }
+
+    changeStatusMatricula(){
+
+    }
+
+    ativarMatricula(){
+        
+    }
+
+    trancarMatricula(){
+        
+    }
+
+    suspenderMatricula(){
+        
+    }
+
+    afastarAluno(){
+        
+    }
+
+    terminarCurso(){
+        
+    }
+
+    jubilarAluno(){
+        
+    }
+}
+
+module.exports = class Ativo extends State {
+    constructor() {
+    
+    }
+
+    changeStatusMatricula(){
+
+    }
+
+    trancarMatricula(){
+        
+    }
+
+    suspenderMatricula(){
+        
+    }
+
+    afastarAluno(){
+        
+    }
+
+    terminarCurso(){
+        
+    }
+
+    jubilarAluno(){
+        
+    }
+}
+
+module.exports = class MatriculaTrancada extends State {
+    constructor() {
+    
+    }
+
+    changeStatusMatricula(){
+
+    }
+
+    ativarMatricula(){
+        
+    }
+
+    suspenderMatricula(){
+        
+    }
+}
+
+module.exports = class Suspenso extends State {
+    constructor() {
+    
+    }
+
+    changeStatusMatricula(){
+
+    }
+
+    ativarMatricula(){
+        
+    }
+
+    trancarMatricula(){
+        
+    }
+
+    afastarAluno(){
+        
+    }
+}
+
+module.exports = class Afastado extends State {
+    constructor() {
+    
+    }
+
+    changeStatusMatricula(){
+
+    }
+
+    ativarMatricula(){
+        
+    }
+
+    trancarMatricula(){
+        
+    }
+
+    suspenderMatricula(){
+        
+    }
+}
+
+module.exports = class Formado extends State {
+    constructor() {
+    
+    }
+
+    changeStatusMatricula(){
+
+    }
+}
+
+module.exports = class Jubilado extends State {
+    constructor() {
+    
+    }
+
+    changeStatusMatricula(){
+
+    }
+
+    ativarMatricula(){
+        
+    }
+
+    trancarMatricula(){
+        
+    }
+
+    suspenderMatricula(){
+        
+    }
+
+    afastarAluno(){
+        
+    }
+}
