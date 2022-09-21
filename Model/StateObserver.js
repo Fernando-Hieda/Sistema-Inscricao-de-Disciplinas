e0bb2a0d0c70310e30516b7a0861561beb900407
brunomatosdesouza <107889891+brunomatosdesouza@users.noreply.github.com>
2022-09-21 18:29:27 -0300

    parte da implementação do observer

--- Model/StateObserver.js
+++ Model/StateObserver.js
@@ -2,7 +2,24 @@ var Aluno = require("./Aluno.js")
 
 module.exports = class StateObserver {
     constructor() {
+        this.alunos = []
     
+    }
+
+    subscribe(idAluno) {
+        let aluno = db.get('alunos').find({id : idAluno}).value()
+        this.alunos.push(aluno)
+    }
+
+    unsubscribe(idAluno) {
+        for(var i = 0; i < this.alunos.length; i++) {
+            if(idAluno == alunos[i].id) {
+                alunos[i] = null
+                for(;i < this.alunos.length - 1;) {
+                    alunos[i] = alunos[i++]
+                }
+            }
+        }
     }
 
     update(msg) {
@@ -10,7 +27,7 @@ module.exports = class StateObserver {
     }    
 }
 
-module.exports = class ConcreteStateObserver extends StateObserver {
+/* module.exports = class ConcreteStateObserver extends StateObserver {
     constructor() {
     
     }
@@ -18,4 +35,4 @@ module.exports = class ConcreteStateObserver extends StateObserver {
     update(msg) {
         return msg
     }    
-}
+}*/
